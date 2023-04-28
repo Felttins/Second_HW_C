2c66c54fe269551c10d1062aabfad3d750458106
Lebedev Vitaly <11132166@mail.ru>
2023-04-28 22:50:20 +0300

    Сделал 15 задание

---
+++ Task 15/Program.cs
@@ -0,0 +1,26 @@
+﻿int N = Prompt("");
+
+int Prompt(string message)
+{
+    Console.Write("Введите число - ");
+    int N= int.Parse(Console.ReadLine()!);
+    return N;
+}
+
+Operation(N);
+
+void Operation(int number)
+{
+    if (number > 0 && number < 6)
+    {
+        Console.WriteLine("Сегодня рабочий день!");
+    }
+    else if(number > 5 && number < 8)
+    {
+        Console.WriteLine("Сегодня ВЫХОДНОЙ");
+    }
+    else
+    {
+       Console.WriteLine("А Вы уверены что такой день существует?");
+    }
+}
