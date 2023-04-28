f1dbe0b32b63970bacb1c8bbf17b907bfdd02aff
Lebedev Vitaly <11132166@mail.ru>
2023-04-28 22:26:36 +0300

    Решил 10 задачу

---
+++ Task 10/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int N = Prompt(" ");
+
+int Prompt(string message)
+{
+    Console.Write("Введите трёхзначное число- ");
+    int N= int.Parse(Console.ReadLine()!);
+    return N;
+}
+
+Operation(N);
+
+void Operation(int number)
+{
+    if (number >100 && number < 1000)
+    {
+        Console.WriteLine(number/10%10);
+    }
+    else
+    {
+        Console.WriteLine("Данное число не трёхзначно!");
+    }
+}
